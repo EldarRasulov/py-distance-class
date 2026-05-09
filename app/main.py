48ca20e1c55b5db7829c10fb3cdae643771e7f5d
Eldar RasuLov <morddiff@gmail.com>
2026-05-09 23:16:15 +0400

    distance-new-try 0.1

--- app/main.py
+++ app/main.py
@@ -46,6 +46,7 @@ class Distance:
             return self.km <= other.km
         return self.km <= other
 
+
     def __ge__(self, other):
         if isinstance(other, Distance):
             return self.km >= other.km
